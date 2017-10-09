58dffd03a237bcc9d7d064c62d9a1cf6f60328ac
femio23 <chiscazo.paco@gmail.com>
2017-10-09 08:56:32 +0200

    Primeiro codigo

---
+++ Boletin2_3/src/boletin2_3/Boletin2_3.java
@@ -0,0 +1,18 @@
+
+package boletin2_3;
+import java.util.Scanner;
+public class Boletin2_3 {
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);//declaramos variable escaner.
+        float e,cd,usd;
+        System.out.println("Introduzca la cantidad de euros.");
+        e= sc.nextFloat();//invocamos escaner, para que introduzca un float.
+        System.out.println("Introduzca el tipo de cambio.");
+        cd = sc.nextFloat();
+        usd=e*cd;
+        System.out.println(e+"euros son "+usd+"dolares");
+    }
+    
+}
