0efdc7839ab36f211cd19043301016ca103779de
Femio23 <chiscazo.paco@gmail.com>
2017-10-20 20:06:43 +0200

    Castellanizado.

--- Boletin2_2/src/boletin2_2/Boletin2_2.java
+++ Boletin2_2/src/boletin2_2/Boletin2_2.java
@@ -8,7 +8,7 @@ public class Boletin2_2 {
     public static void main(String[] args) {
         float L =3f,a;
         a=L*L;
-        System.out.println("Area = "+a);
+        System.out.println("La Area = "+a);
     }
     
 }
